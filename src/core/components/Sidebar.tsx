be7713b5b5e329e659ab03637bf02ae5c5ad5fd6
Rozita Hasani <rozita.hasani74@gmail.com>
2024-09-26 09:20:53 +0200

    refactor: create NavigationLink component

--- src/core/components/Sidebar.tsx
+++ src/core/components/Sidebar.tsx
@@ -1,49 +1,43 @@
 import React, {useContext, useState} from 'react';
 import clsx from 'clsx';
-import {useNavigate, useLocation, Link} from 'react-router-dom';
+import {useLocation, Link} from 'react-router-dom';
 import {Logo} from '.';
-import {House, ChartPie, Settings, Building, Bell, User, Users, Clock, TreePalm} from 'lucide-react';
+import {House, ChartPie, Settings, Building, Bell, User, Users, Clock, TreePalm, LucideProps} from 'lucide-react';
 import {Button} from "@/components/ui/button";
 import {UserContext} from "@/contexts/UserContext";
 import {Badge} from "@/components/ui/badge"
+import * as react from "react";
+
+const mainNavigation = [
+    {name: 'Home', href: '/', icon: House},
+    {name: 'Balance', href: '/balance', icon: ChartPie},
+    {name: 'Settings', href: '/settings', icon: Settings},
+];
+
+const managementNavigation = [
+    {name: 'Requests', href: '/requests', icon: Clock, length: 3},
+    {name: 'Organization', href: '/organization', icon: Building},
+    {name: 'Policy', href: '/policy', icon: TreePalm},
+    {name: 'Employees', href: '/employees', icon: User},
+    {name: 'Teams', href: '/teams', icon: Users},
+];
 
 export default function Sidebar() {
     const [isSheetOpen, setSheetOpen] = useState(false);
     const [selectedOption, setSelectedOption] = useState<string>('');
     const location = useLocation();
-    const navigate = useNavigate();
     const {user} = useContext(UserContext);
-
-    const mainNavigation = [
-        {name: 'Home', href: '/', icon: House},
-        {name: 'Balance', href: '/balance', icon: ChartPie},
-        {name: 'Settings', href: '/settings', icon: Settings},
-    ];
-
-    const managementNavigation =
-        user?.role === 'ORGANIZATION_ADMIN'
-            ? [
-                {name: 'Requests', href: '/requests', icon: Clock, length: 3},
-                {name: 'Organization', href: '/organization', icon: Building},
-                {name: 'Policy', href: '/policy', icon: TreePalm},
-                {name: 'Employees', href: '/employees', icon: User},
-                {name: 'Teams', href: '/teams', icon: Users},
-            ]
-            : [];
 
     const handleNavigation = (path: string) => {
         setSelectedOption(path);
         setSheetOpen(false);
-        navigate(path);
     };
 
-    const classes = {
-        base: 'flex items-center gap-3 rounded-lg px-3 py-2 transition-all',
-        active: 'bg-indigo-100 dark:bg-indigo-800 bg-opacity-75',
-        inactive: 'text-muted-foreground hover:text-primary',
+    const isNavigationActive = (href: string) => {
+        return location.pathname === href || selectedOption === href;
     };
-    const getLinkClasses = (isActive: boolean) =>
-        clsx(classes.base, isActive ? classes.active : classes.inactive);
+
+    const isUserAdmin = user?.role === 'ORGANIZATION_ADMIN';
 
     return (
         <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
@@ -62,49 +56,67 @@ export default function Sidebar() {
 
                     <div className="flex-1">
                         <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                            {mainNavigation.map((item) => {
-                                const isActive = location.pathname === item.href || selectedOption === item.href;
-                                return (
-                                    <div key={item.name} className="mb-1">
-                                        <Link
-                                            to={item.href}
-                                            onClick={() => handleNavigation(item.href)}
-                                            className={getLinkClasses(isActive)}
-                                        >
-                                            <item.icon className="h-4 w-4" aria-hidden="true"/>
-                                            {item.name}
-                                        </Link>
-                                    </div>
-                                );
-                            })}
+                            {mainNavigation.map((item) =>
+                                <NavigationLink
+                                    href={item.href}
+                                    title={item.name}
+                                    isActive={isNavigationActive(item.href)}
+                                    badge={null}
+                                    onClick={handleNavigation}
+                                    Icon={item.icon}/>
+                            )}
 
-                            {user?.role === 'ORGANIZATION_ADMIN' && (
+                            {isUserAdmin && (
                                 <>
-                                    <div className="px-3 text-[10px] text-muted-foreground uppercase tracking-wide mt-4 mb-2">Management</div>
-                                    {managementNavigation.map((item) => {
-                                        const isActive = location.pathname === item.href || selectedOption === item.href;
-                                        return (
-                                            <div key={item.name} className="mb-1">
-                                                <Link
-                                                    to={item.href}
-                                                    onClick={() => handleNavigation(item.href)}
-                                                    className={getLinkClasses(isActive)}
-                                                >
-                                                    <item.icon className="h-4 w-4" aria-hidden="true"/>
-                                                    {item.name}
-                                                    {item.length && (
-                                                        <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">{item.length}</Badge>
-                                                    )}
-                                                </Link>
-                                            </div>
-                                        );
-                                    })}
+                                    <div
+                                        className="px-3 text-[10px] text-muted-foreground uppercase tracking-wide mt-4 mb-2">Management
+                                    </div>
+                                    {managementNavigation.map((item) =>
+                                        <NavigationLink
+                                            href={item.href}
+                                            title={item.name}
+                                            isActive={isNavigationActive(item.href)}
+                                            badge={item.length}
+                                            onClick={handleNavigation}
+                                            Icon={item.icon}/>
+                                    )}
                                 </>
                             )}
                         </nav>
                     </div>
                 </div>
             </div>
+        </div>
+    );
+}
+
+type NavigationLinkProps = {
+    href: string;
+    title: string;
+    isActive: boolean;
+    badge: number | null,
+    onClick: (page: string) => void;
+    Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+function NavigationLink({href, title, isActive, onClick, badge, Icon}: NavigationLinkProps) {
+    const classes = {
+        base: 'flex items-center gap-3 rounded-lg px-3 py-2 transition-all',
+        active: 'bg-indigo-100 dark:bg-indigo-800 bg-opacity-75',
+        inactive: 'text-muted-foreground hover:text-primary',
+    };
+
+    return (
+        <div key={title} className="mb-1">
+            <Link
+                to={href}
+                onClick={() => onClick(href)}
+                className={clsx(classes.base, isActive ? classes.active : classes.inactive)}>
+                <Icon className="h-4 w-4" aria-hidden="true"/>
+                {title}
+                {badge && (<Badge
+                    className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">{badge}</Badge>)}
+            </Link>
         </div>
     );
 }
