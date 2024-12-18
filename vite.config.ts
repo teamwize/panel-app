a303789dafdbe79f71050c1313dc30ba07a4fc10
rozita-hasani <rozita.hasani74@gmail.com>
2024-12-18 10:16:18 +0100

    feat: add proxy configuration to vite for API requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,32 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-  define: {
-    'process.env': {},
-  },
-  resolve: {
-    alias: {
-      '~': "/src",
-      '@': '/src',
+    plugins: [
+        react()
+    ],
+    define: {
+        'process.env': {},
     },
-  },
-  root: '.',
-  build: {
-    outDir: 'dist'
-  }
+    resolve: {
+        alias: {
+            '~': "/src",
+            '@': '/src',
+        },
+    },
+    root: '.',
+    build: {
+        outDir: 'dist'
+    },
+    server: {
+        proxy: {
+            // Proxy all requests starting with '/api'
+            '/api': {
+                target: 'https://api.teamwize.app',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 })
