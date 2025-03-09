49cacb308ac5373e96078d9b648023f8ed35ca58
Rozita Hasani <106513953+rozita-hasani@users.noreply.github.com>
2025-03-09 17:42:47 +0100

    update UserAvatar component (#89)

--- src/modules/user/components/UserList.tsx
+++ src/modules/user/components/UserList.tsx
@@ -34,9 +34,9 @@ export function UserList({
                             className="flex items-center space-x-2 cursor-pointer hover:text-primary transition-colors"
                         >
                             <UserAvatar
-                                avatar={employee?.avatar}
-                                avatarSize={40}
-                                className="border-2 border-muted"
+                                user={employee}
+                                size={46}
+                                className=""
                             />
                             <div className="flex flex-col">
                                 <span
